0f8bb0671799c645c53f450c8db110038036cc8f
Lucy Hutcheson <hutcheson.lucy@gmail.com>
2012-02-13 21:21:55 -0600

    New items for project 4.

---
+++ json.js
@@ -0,0 +1,42 @@
+/**
+ * Project 4 Deliverable
+ * @author Lucy Hutcheson
+ * Date: 2012-02-23
+ * Created for:  Visual Frameworks 1202
+ */
+
+var json = {
+	"lesson1": {
+		"name": ["Lesson Name:", "Pride"],
+		"author": ["Author:", "Jason Hutcheson"],
+		"email": ["Email:", "jason@gmail.com"],
+		"date": ["Date:", "2012-02-09"],
+		"topic": ["Topics:", "Christian Life"],
+		"book": ["Book:", "Mark"],
+		"audience": ["Audience:", "Men"],
+		"length": ["Length:", "90"],
+		"lesson": ["Lesson Text:", "My text for my lesson on pride."]
+	},
+	"lesson2": {
+		"name": ["Lesson Name:", "Compassion"],
+		"author": ["Author:", "Sean Morgan"],
+		"email": ["Email:", "sean@gmail.com"],
+		"date": ["Date:", "2012-02-19"],
+		"topic": ["Topics:", "Christian Life"],
+		"book": ["Book:", "Luke"],
+		"audience": ["Audience:", "Adults"],
+		"length": ["Length:", "90"],
+		"lesson": ["Lesson Text:", "My text for my lesson on compassion."]
+	},
+	"lesson3": {
+		"name": ["Lesson Name:", "Dating"],
+		"author": ["Author:", "Laury Almoguera"],
+		"email": ["Email:", "laury@gmail.com"],
+		"date": ["Date:", "2012-02-11"],
+		"topic": ["Topics:", "Christian Life"],
+		"book": ["Book:", "John"],
+		"audience": ["Audience:", "Youth"],
+		"length": ["Length:", "30"],
+		"lesson": ["Lesson Text:", "My text for my lesson on dating."]
+	}
+}
